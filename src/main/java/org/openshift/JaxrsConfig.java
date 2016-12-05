801ef6a9f9eb57794a955af3c03c7bee782833b2
marmack <michael.s.white@gmail.com>
2016-12-05 16:24:51 -0600

    Add files via upload

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,15 @@
+package org.openshift;
+
+
+
+import javax.ws.rs.ApplicationPath;
+
+import javax.ws.rs.core.Application;
+
+
+
+@ApplicationPath("/api")
+
+public class JaxrsConfig extends Application{
+
+}
